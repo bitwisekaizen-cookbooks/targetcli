cf8347843d3af892848a7e9aa87c5f6a6d856184
Gavin Gray <gavin@infinio.com>
2015-04-24 12:10:47 -0400

    Initial commit.

---
+++ metadata.rb
@@ -0,0 +1,8 @@
+name             'targetcli'
+maintainer       'The Authors'
+maintainer_email 'you@example.com'
+license          'all_rights'
+description      'Installs/Configures targetcli'
+long_description 'Installs/Configures targetcli'
+version          '0.1.0'
+
